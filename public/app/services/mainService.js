3bec5c710e1a0be6f32010be856c172250479531
Rob DelValle <robdel13@gmail.com>
2017-02-02 20:32:54 -0700

    front end and controller

--- public/app/services/mainService.js
+++ public/app/services/mainService.js
@@ -1,3 +1,18 @@
 angular.module('app').service('mainService',function($http){
 
+    var data = [
+        {
+            action: 'going to the store',
+            inProcess: true,
+            completed: false
+        },
+        {
+            action: 'take battery out',
+            inProcess: false,
+            completed: true
+        }
+    ];
+
+    this.getData = data;
+
 });//closing
